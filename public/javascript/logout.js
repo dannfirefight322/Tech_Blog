85a3574c488f0d0dc3ba806ce4ab1a64554aab79
Daniel Burford <amery.bur410@gmail.com>
2021-11-02 15:35:01 -0500

    trying to get logout to function.

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,15 +1,14 @@
-const logout=async(event)=> {
-  event.preventDefault();
-    const response = async('/api/users/logout', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to log out.');
   }
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
